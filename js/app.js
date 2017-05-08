77a132089eefb1fa619788016e23e4f5073f4614
igorfachini <igor.fachini@catolicasc.org.br>
2017-05-08 13:21:27 -0300

    firebase and dataManager init

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+//<script src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB7krgsbX6gZQaWsAGoaYISkB5uAkMK3po",
+  authDomain: "rpgmaker-39ce8.firebaseapp.com",
+  databaseURL: "https://rpgmaker-39ce8.firebaseio.com",
+  projectId: "rpgmaker-39ce8",
+  storageBucket: "rpgmaker-39ce8.appspot.com",
+  messagingSenderId: "94934084477"
+};
+firebase.initializeApp(config);
